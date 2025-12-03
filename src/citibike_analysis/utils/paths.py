e9d9417dc67f5445f7287e2054657fa1fd1caf3f
Sayyam Alam <sayyam@gmx.de>
2025-12-03 11:31:23 +0100

    Update paths

--- src/citibike_analysis/utils/paths.py
+++ src/citibike_analysis/utils/paths.py
@@ -10,6 +10,7 @@ DATA_RAW = os.path.join(DATA_DIR, "data_raw")
 DATA_PROCESSED = os.path.join(DATA_DIR, "data_processed")
 DATA_SPATIAL = os.path.join(DATA_DIR, "data_spatial")
 DATA_TEMPORAL = os.path.join(DATA_DIR, "data_temporal")
+DATA_HYPOTHESIS = os.path.join(DATA_DIR, "data_hypothesis")
 
 # Abbildungsverzeichnis
 FIGURES_DIR = os.path.join(PROJECT_ROOT, "figures")
